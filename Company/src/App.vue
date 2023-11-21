5280283879bc4b272632b0071530366e0665195c
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2023-11-21 11:00:39 +0700

    micro-frontend

---
+++ Company/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <Header/>
+    Company Project
+</template>
+
+<script>
+import Header from './Header.vue'
+export default {
+    components:{
+        Header
+    }
+    
+}
+</script>
---
+++ Shop/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <Header/>
+    <h2>Shop Project</h2>
+    <div>
+        <button @click="addItem">Add item</button>
+    </div>
+</template>
+
+<script>
+import Header from 'Company/Header';
+export default {
+    components:{
+        Header
+    },
+    methods: {
+         addItem() {
+             this.$store.state.cartItems += 1
+         }
+     },  
+}
+</script>
